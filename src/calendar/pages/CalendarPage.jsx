19b2a3e973be6c396f441058ccfeae89254d31bf
z0s3r77 <ipopdue24@gmail.com>
2023-10-19 16:42:59 +0200

    Localizer implementado

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,39 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
+import { NavBar } from "../components/Navbar";
+
+import { localizer } from '../../helpers';
+
+
+
+
+const events = [{
+    title: 'Cumpleaños del Jege',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Sebastian'
+    }
+
+}]
+export const CalendarPage = () => {
+    return (
+
+        <>
+            <NavBar />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+            />
+        </>
+
+    )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes} from "react-router-dom"
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+
+
+
+
+export const AppRouter = () => {
+
+    const authStatus = 'authenticated';
+
+    return (
+        <Routes>
+            {
+                 (authStatus === 'non-authenticated') 
+                 ?            <Route path="/auth/*" element={<LoginPage/>}/>
+                :             <Route path="/*" element={ <CalendarPage/> }/>
+
+            }
+            <Route path="/*" element={ <Navigate to="/auth/login"/> }/>
+        </Routes>
+    )
+
+}
